f8a559003c7b3daa745384a9dbe5bb6b25cd7edf
khadija-baidi <baidikhadija3@gmail.com>
2025-05-08 22:53:11 +0100

    menu

---
+++ public/js/app.js
@@ -0,0 +1,58 @@
+
+// menu
+
+const allTabsBtn = document.getElementById('all-tabs')
+const startersBtn = document.getElementById('starters')
+const saladesBtn = document.getElementById('salades')
+const specialtyBtn = document.getElementById('specialty')
+
+
+const allPlats = document.querySelectorAll('.plats')
+
+
+allPlats.forEach(plat => {
+  plat.style.display = 'flex'; 
+})
+
+
+allTabsBtn.addEventListener('click', () => {
+  allPlats.forEach(plat => {
+    plat.style.display = 'flex';
+  })
+})
+
+
+startersBtn.addEventListener('click', () => {
+  allPlats.forEach(plat => {
+    if (plat.classList.contains('starters-plat')) {
+      plat.style.display = 'flex';
+    } else {
+      plat.style.display = 'none';
+    }
+  })
+})
+
+
+saladesBtn.addEventListener('click', () => {
+  allPlats.forEach(plat => {
+    if (plat.classList.contains('salades')) {
+      plat.style.display = 'flex';
+    } else {
+      plat.style.display = 'none';
+    }
+  })
+})
+
+
+specialtyBtn.addEventListener('click', () => {
+  allPlats.forEach(plat => {
+    if (plat.classList.contains('specialty')) {
+      plat.style.display = 'flex';
+    } else {
+      plat.style.display = 'none';
+    }
+  })
+})
+
+
+
